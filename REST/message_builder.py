7b0aea904091f3a67064e0e346ef654ae4a8eecc
rmu <(nrg_8Vv{z>
2019-09-03 15:56:08 +0200

    first test between classes

---
+++ REST/message_builder.py
@@ -0,0 +1,14 @@
+"""
+Module defines help methods for sending REST messages to Server
+
+Author: R. Mueller 03.09.2019
+
+"""
+
+
+class RESTMessageBuilder(object):
+    def __init__(self):
+        pass
+
+    def send_status(self, message):
+        print(message)
---
+++ Tools/backup_openhabserver.py
@@ -0,0 +1,4 @@
+from REST.message_builder import RESTMessageBuilder
+
+rest_builder = RESTMessageBuilder()
+rest_builder.send_status(message="test")

9c9851522810b8f6ca4575bc887a427e28b6abcd
raikm <raik.mueller@copra-system.de>
2020-05-29 09:47:54 +0200

    function for api interaction provided

---
+++ Tools/gmail_reader.py
@@ -0,0 +1,6 @@
+from package_tracker_manager import get_couriers
+
+
+couriers = get_couriers()
+print(couriers)
+
--- Tools/package_tracker_manager.py
+++ Tools/package_tracker_manager.py
@@ -5,9 +5,34 @@ configParser = configparser.RawConfigParser()
 configFilePath = r'../config.txt'
 configParser.read(configFilePath)
 
-test = configParser.get('aftership', 'api_key')
-print(test)
+api_key = configParser.get('aftership', 'api_key')
+aftership.api_key = api_key
+
+
+def get_couriers():
+    return aftership.courier.list_couriers()
+
 
 # create package
+def create_tracking(slug, tracking_number):
+    tracking = {'slug': slug, 'tracking_number': tracking_number}
+    result = aftership.tracking.create_tracking(tracking=tracking, timeout=10)
+    return result['tracking']['id']
+
 
 # delete tracking
+def get_tracking(tracking_id, fields=None):
+    try:
+        result = aftership.tracking.get_tracking(tracking_id=tracking_id, fields=','.join(fields))
+    except aftership.exception.NotFound:
+        return None
+
+    return result['tracking']
+
+
+def delete_tracking(tracking_id):
+    try:
+        aftership.tracking.delete_tracking(tracking_id=tracking_id)
+    except aftership.exception.NotFound:
+        return False
+    return True

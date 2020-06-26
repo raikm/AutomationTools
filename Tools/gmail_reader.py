fb6ae92153c118fa419778bf2a97ea616230beff
raikm <raik.mueller@copra-system.de>
2020-06-26 19:55:48 +0200

    bugfixes for gmail reader and delete messages

--- Tools/gmail_reader.py
+++ Tools/gmail_reader.py
@@ -4,14 +4,20 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from Tools.package_tracker_manager import get_couriers, create_tracking, update_tracking
+from Aftership.package_tracker_manager import get_couriers, create_tracking, update_tracking
 from SQLite.db_handler import DBHandler
 import re
+import aftership
+from apiclient import errors
+from REST.message_builder import RESTMessageBuilder
+
+
+mb = RESTMessageBuilder()
 
 credentials_path = "../Resources/credentials.json"
 token_path = "../Resources/token.pickle"
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ['https://mail.google.com/']
 
 
 def connect_to_gmail_api():
@@ -84,7 +90,8 @@ def get_forwared_message(msg):
 
 def get_msg_id(msg):
     try:
-        return service.users().messages().get(userId='me', id=msg['id']).execute()
+        msg = service.users().messages().get(userId='me', id=msg['id']).execute()
+        return msg['id']
     except:
         pass
 
@@ -131,7 +138,11 @@ def get_trackingnumber(company_name, msg_body, supplier):
     if company_name != "N.A.":
         hint = db.get_sender_hint(company_name)
     else:
-        hint = db.get_supplier_hint(supplier)
+        try:
+            hint = db.get_supplier_hint(supplier)
+        except:
+            print("TODO ERROR")
+            return None
     general_hints = ["Sendnungsnummer", "Trackingnummer", "Sendungsverfolgungsnummer", "trackingnumber", "Paket", "Paketverfolgungsnummern", "AMAZON-Paket"]
     msg_body = msg_body.replace("\n","").replace("\r","")
     if hint:
@@ -162,35 +173,45 @@ def get_trackingnumber(company_name, msg_body, supplier):
 def delete_message(msg_id):
     try:
         service.users().messages().delete(userId='me', id=msg_id).execute()
-    except:
-        pass
-    pass
+        print('Message with id: %s deleted successfully.' % msg_id)
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
 
 
 if __name__ == '__main__':
-    couriers = get_couriers()
-    db = DBHandler("../SQLite/database.db")
-    service = connect_to_gmail_api()
-    messages = get_messages(service)
+    try:
+        couriers = get_couriers()
+        db = DBHandler("../SQLite/database.db")
+        service = connect_to_gmail_api()
+        messages = get_messages(service)
 
 
-    for message in messages:
-        sender = get_sender(message)
-        supplier = get_supplier(sender, message)
-        content = get_forwared_message(message)
-        if content == "error":
-            continue
-        else:
-            tracking_number = get_trackingnumber(sender, content, supplier)
-            if tracking_number is not None and supplier != "N.A.":
-                #tid = create_tracking(slug=supplier, tracking_number= str(tracking_number))
-                #update_tracking(tid, title=str(sender))
-                print(supplier + "     :     " + str(tracking_number))
-            elif tracking_number is not None:
-                pass
-                #TODO: try with main suppliers
+        for message in messages:
+            sender = get_sender(message)
+            supplier = get_supplier(sender, message)
+            content = get_forwared_message(message)
+            if content == "error":
+                continue
+            else:
+                tracking_number = get_trackingnumber(sender, content, supplier)
+                if tracking_number is not None and supplier != "N.A.":
+                    try:
+                        tid = create_tracking(slug=supplier, tracking_number= str(tracking_number))
+                        update_tracking(tid, title=str(sender))
+                        print(supplier + "     :     " + str(tracking_number))
+                        delete_message(get_msg_id(message))
+                    except aftership.exception.BadRequest:
+                        continue
+                elif tracking_number is not None:
+                    pass
+                    #TODO:
+        mb.send_status_to_server(script_path=__file__, result=mb.successful,
+                                 error_message="")
+    except Exception as e:
+        mb.send_status_to_server(script_path=__file__, result=mb.fail,
+                             error_message=str(e))
 
 
 
 
-            #delete_message(get_msg_id(message))
+
